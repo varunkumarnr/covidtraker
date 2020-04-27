1aaf400c7511439fab9ff00c749323e05c612ec9
varunkumarnr <varunnrk@gmail.com>
2020-04-27 20:42:31 +0530

    Updates

---
+++ precache-manifest.7e4f5ac6d9acc4e7a820d33147340b91.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "443efddc4e4f7436a30d093711c5055a",
+    "url": "/covidtraker/index.html"
+  },
+  {
+    "revision": "23e54133275c3b3ee956",
+    "url": "/covidtraker/static/css/main.97ab6017.chunk.css"
+  },
+  {
+    "revision": "5e7c413812f18927b2d3",
+    "url": "/covidtraker/static/js/2.ae7ffea3.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covidtraker/static/js/2.ae7ffea3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23e54133275c3b3ee956",
+    "url": "/covidtraker/static/js/main.775bee0f.chunk.js"
+  },
+  {
+    "revision": "723f9b09b5d888f30601",
+    "url": "/covidtraker/static/js/runtime-main.324512e7.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covidtraker/static/media/image.d7265326.png"
+  }
+]);
